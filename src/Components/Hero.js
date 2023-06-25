128ec93c874fd3269f9838023dc20e5cc6fa61ef
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-06-26 04:37:31 +0530

    :fire: Major Improvements :white_check_mark:

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -12,7 +12,21 @@ export const Hero = () => {
               We are a nonprofit organisation and work worldwide
             </h1>
             <Link to="/products">
-              <button className="shop-now">SHOP NOW</button>
+              <button className="shop-now">
+                SHOP NOW
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="link-icon"
+                >
+                  <path d="M12 5l7 7-7 7" />
+                </svg>
+              </button>
             </Link>
           </div>
         </div>
