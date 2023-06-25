b9b1a3bb06199ae8f56cd88b4ba3cedfb5900784
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-06-26 02:33:18 +0530

    :fire: Major Improvements :white_check_mark:

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar } from './Navbar'
 import { Hero } from './Hero'
- 
+import { About } from './About'
+
 export const Landing = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   )
 }
