0adec301561c30f54ce59889121ead2ade0da746
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-08-17 17:33:10 +0530

    :fire: fixed add-to-cart :bug:

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,50 +1,59 @@
 import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import { ProductsContext } from "../Global/ProductsContext";
 import { CartContext } from "../Global/CartContext";
 import { Loader } from "./Loader";
+import { useAuth } from "../Global/AuthContext"; // Assuming you have the AuthContext set up
 
 export const Products = () => {
   const { products } = useContext(ProductsContext);
-
   const { dispatch } = useContext(CartContext);
+  const { currentUser, login } = useAuth(); // Using the useAuth hook from your AuthContext
+
+  const history = useHistory();
+
+  const addToCart = (product) => {
+    if (currentUser) {
+      dispatch({
+        type: "ADD_TO_CART",
+        id: product.ProductID,
+        product,
+      });
+    } else {
+      // Redirect to login and perform the login action after successful login
+      history.push("/login");
+    }
+  };
 
   return (
-    <>
-      <div className="products-main-container">
-        <div className="products-wrapper">
-          {products.length !== 0 && <h1>Products</h1>}
-          <div className="products-container">
-            {products.length === 0 && (
-              <div>
-                <Loader />
+    <div className="products-main-container">
+      <div className="products-wrapper">
+        {products.length !== 0 && <h1>Products</h1>}
+        <div className="products-container">
+          {products.length === 0 && (
+            <div>
+              <Loader />
+            </div>
+          )}
+          {products.map((product) => (
+            <div className="product-card" key={product.ProductID}>
+              <div className="product-img">
+                <img src={product.ProductImg} alt="not found" />
               </div>
-            )}
-            {products.map((product) => (
-              <div className="product-card" key={product.ProductID}>
-                <div className="product-img">
-                  <img src={product.ProductImg} alt="not found" />
-                </div>
-                <div className="product-name">{product.ProductName}</div>
-                <div className="product-price">
-                  Rs {product.ProductPrice}.00
-                </div>
-                <button
-                  className="addcart-btn"
-                  onClick={() =>
-                    dispatch({
-                      type: "ADD_TO_CART",
-                      id: product.ProductID,
-                      product,
-                    })
-                  }
-                >
-                  ADD TO CART
-                </button>
+              <div className="product-name">{product.ProductName}</div>
+              <div className="product-price">
+                Rs {product.ProductPrice}.00
               </div>
-            ))}
-          </div>
+              <button
+                className="addcart-btn"
+                onClick={() => addToCart(product)}
+              >
+                ADD TO CART
+              </button>
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import { AuthProvider } from './Global/AuthContext';
 
 ReactDOM.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
 
-  <App />,
-  
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
